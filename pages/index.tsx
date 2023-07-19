0650ba4eaa15c9c3eff0524050a4eca607057f08
hylerrix <hylerrix@gmail.com>
2023-07-19 23:54:59 +0800

    refactor: material->blocks, use prettier to format

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,43 @@
 import Head from "next/head"
 
+import NavHeader from "blocks/NavHeader"
 import LandingPage from "./landing-page"
-import NavHeader from "material/NavHeader"
 
 export default function Web() {
   return (
     <>
       <Head>
-        <meta charSet="utf-8"/>
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
-        <meta name="description" content="We’re on a journey to advance and democratize artificial intelligence through open source and open science."/>
-        <meta property="fb:app_id" content="1321688464574422"/>
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:site" content="@huggingface"/>
-        <meta property="og:title" content="Hugging Face – The AI community building the future."/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://huggingface.co/"/>
-        <meta property="og:image" content="https://huggingface.co/front/thumbnails/v2-2.png"/>
-        <link rel="stylesheet" href="/front/build/kube/style.css"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600;700&display=swap" rel="stylesheet"/>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"/>
+        <meta
+          name="description"
+          content="We’re on a journey to advance and democratize artificial intelligence through open source and open science."
+        />
+        <meta property="fb:app_id" content="1321688464574422" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@huggingface" />
+        <meta property="og:title" content="Hugging Face – The AI community building the future." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://huggingface.co/" />
+        <meta property="og:image" content="https://huggingface.co/front/thumbnails/v2-2.png" />
+        <link rel="stylesheet" href="/front/build/kube/style.css" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" />
         {/* <!-- HEAD_svelte-vwinwk_START --> */}
         {/* <link rel="alternate" type="application/rss+xml" href="/blog/feed.xml" title="Hugging Face Blog" /> */}
         {/* <!-- HEAD_svelte-vwinwk_END --> */}
         <title>Hugging Face – The AI community building the future.</title>
         <script defer data-domain="huggingface.co" src="/js/script.js"></script>
       </Head>
-      <div className="flex flex-col min-h-screen bg-white dark:bg-gray-950 text-black HomePage">
+      <div className="HomePage flex min-h-screen flex-col bg-white text-black dark:bg-gray-950">
         <div className="flex min-h-screen flex-col">
           <NavHeader />
           <div className="SVELTE_HYDRATER contents" data-props="{}" data-target="GoogleAnalyticsTracker"></div>
--- pages/landing-page/LandingPageFooter.tsx
+++ pages/landing-page/LandingPageFooter.tsx
@@ -1,28 +1,40 @@
 export default function LandingPageFooter() {
   return (
     <footer className="border-t border-gray-100">
-      <div className="container pt-12 pb-32">
+      <div className="container pb-32 pt-12">
         <div className="grid gap-8 sm:grid-cols-2 md:grid-cols-4">
           <div>
             <div className="mb-4 text-lg font-semibold">Website</div>
             <ul className="space-y-1 text-gray-600 md:space-y-2">
               <li>
-                <a className="hover:underline" href="/models">Models </a>
+                <a className="hover:underline" href="/models">
+                  Models{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/datasets">Datasets </a>
+                <a className="hover:underline" href="/datasets">
+                  Datasets{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/spaces">Spaces </a>
+                <a className="hover:underline" href="/spaces">
+                  Spaces{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/tasks">Tasks </a>
+                <a className="hover:underline" href="/tasks">
+                  Tasks{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/inference-endpoints">Expert Support </a>
+                <a className="hover:underline" href="/inference-endpoints">
+                  Expert Support{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/support">Expert Acceleration Program </a>
+                <a className="hover:underline" href="/support">
+                  Expert Acceleration Program{" "}
+                </a>
               </li>
             </ul>
           </div>
@@ -30,25 +42,39 @@ export default function LandingPageFooter() {
             <div className="mb-4 text-lg font-semibold">Company</div>
             <ul className="space-y-1 text-gray-600 md:space-y-2">
               <li>
-                <a className="hover:underline" href="/huggingface">About </a>
+                <a className="hover:underline" href="/huggingface">
+                  About{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/shop">HF Store </a>
+                <a className="hover:underline" href="/shop">
+                  HF Store{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/brand">Brand assets </a>
+                <a className="hover:underline" href="/brand">
+                  Brand assets{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/terms-of-service">Terms of service </a>
+                <a className="hover:underline" href="/terms-of-service">
+                  Terms of service{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/privacy">Privacy </a>
+                <a className="hover:underline" href="/privacy">
+                  Privacy{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="https://apply.workable.com/huggingface/">Jobs </a>
+                <a className="hover:underline" href="https://apply.workable.com/huggingface/">
+                  Jobs{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="mailto:press@huggingface.co">Press </a>
+                <a className="hover:underline" href="mailto:press@huggingface.co">
+                  Press{" "}
+                </a>
               </li>
             </ul>
           </div>
@@ -56,19 +82,29 @@ export default function LandingPageFooter() {
             <div className="mb-4 text-lg font-semibold">Resources</div>
             <ul className="space-y-1 text-gray-600 md:space-y-2">
               <li>
-                <a className="hover:underline" href="/learn">Learn </a>
+                <a className="hover:underline" href="/learn">
+                  Learn{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/docs">Documentation </a>
+                <a className="hover:underline" href="/docs">
+                  Documentation{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="/blog">Blog </a>
+                <a className="hover:underline" href="/blog">
+                  Blog{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="https://discuss.huggingface.co">Forum </a>
+                <a className="hover:underline" href="https://discuss.huggingface.co">
+                  Forum{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="https://status.huggingface.co/">Service Status </a>
+                <a className="hover:underline" href="https://status.huggingface.co/">
+                  Service Status{" "}
+                </a>
               </li>
             </ul>
           </div>
@@ -76,24 +112,38 @@ export default function LandingPageFooter() {
             <div className="mb-4 text-lg font-semibold">Social</div>
             <ul className="space-y-1 text-gray-600 md:space-y-2">
               <li>
-                <a className="hover:underline" href="https://github.com/huggingface">GitHub </a>
-              </li>
-              <li>
-                <a className="hover:underline" href="https://twitter.com/huggingface">Twitter </a>
-              </li>
-              <li>
-                <a className="hover:underline" href="https://www.linkedin.com/company/huggingface/">LinkedIn
+                <a className="hover:underline" href="https://github.com/huggingface">
+                  GitHub{" "}
                 </a>
               </li>
               <li>
-                <a className="hover:underline" href="/join/discord">Discord </a>
+                <a className="hover:underline" href="https://twitter.com/huggingface">
+                  Twitter{" "}
+                </a>
               </li>
               <li>
-                <a className="hover:underline" href="https://www.zhihu.com/org/huggingface">Zhihu </a>
+                <a className="hover:underline" href="https://www.linkedin.com/company/huggingface/">
+                  LinkedIn
+                </a>
               </li>
               <li>
-                <a target="_blank" className="hover:underline"
-                  href="https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/blog/chinese-language-blog/wechat.jpg">WeChat
+                <a className="hover:underline" href="/join/discord">
+                  Discord{" "}
+                </a>
+              </li>
+              <li>
+                <a className="hover:underline" href="https://www.zhihu.com/org/huggingface">
+                  Zhihu{" "}
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  className="hover:underline"
+                  href="https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/blog/chinese-language-blog/wechat.jpg"
+                  rel="noreferrer"
+                >
+                  WeChat
                 </a>
               </li>
             </ul>
